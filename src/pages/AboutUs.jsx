cbb16e8c405907bf09db4e440d747ead21f1dbbe
zwolfio <113005910+zwolfio@users.noreply.github.com>
2023-05-13 00:04:19 +0700

    Update AboutUs.jsx

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/navbar/navbar";
+import Navbar from "../components/Navbar/Navbar";
 import About from "../components/About/About"
 import Footer from "../components/Footer/Footer";
 
